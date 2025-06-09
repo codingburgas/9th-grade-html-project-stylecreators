b72648dce6715e0bb0a6067e6d4497f7d80db437
GGYankov23 <GGYakov23@codingburgas.bg>
2025-06-09 22:38:13 +0300

    Add fire chart

---
+++ StyleCreators.Code/JAVASCRIPT/infoChart.js
@@ -0,0 +1,21 @@
+const ctx = document.getElementById('firesChart').getContext('2d');
+        const firesChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                datasets: [{
+                    label: 'Number of Fires',
+                    data: [10, 12, 18, 26, 39, 54, 68, 81, 54, 31, 22, 19],
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
