9477324011a2a976057d05acefd47e5c609780a3
SAVassilev23 <SAVassilev23@codingburgas.bg>
2025-06-10 23:52:59 +0300

    Make general site changes

--- StyleCreators.Code/JAVASCRIPT/infoChart.js
+++ StyleCreators.Code/JAVASCRIPT/infoChart.js
@@ -2,9 +2,9 @@ const ctx = document.getElementById('firesChart').getContext('2d');
         const firesChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                labels: ['Януари', 'Февруари', 'Март', 'Април', 'Май', 'Юни', 'Юли', 'August', 'Септември', 'Октомври', 'Ноември', 'Декември'],
                 datasets: [{
-                    label: 'Number of Fires',
+                    label: 'Брой пожари',
                     data: [10, 12, 18, 26, 39, 54, 68, 81, 54, 31, 22, 19],
                     backgroundColor: 'rgba(255, 99, 132, 0.2)',
                     borderColor: 'rgba(255, 99, 132, 1)',
